d5364624cbc8c62383fefa89cd065b566211cdda
Javier Jaimes <javierjaimes2003@gmail.com>
2023-11-01 16:44:50 -0300

    se cambio nuevamente

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://18.226.169.0:3000/',
+    baseURL: 'http://18.226.169.0:3000/api',
     withCredentials: true
 })
 
